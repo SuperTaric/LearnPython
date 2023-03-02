21dcd99eaa79aa2cadab94a902c70479bee3acd7
tmfed2017 <wangfulin@titanmatrix.com>
2023-03-02 15:51:29 +0800

    添加requirements.txt

--- automatic/juejin.py
+++ automatic/juejin.py
@@ -99,7 +99,7 @@ def run():
 
 def log(text):
     print(text)
-    with open('./log.txt', mode='a') as f:
+    with open('./automatic.log', mode='a') as f:
         f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ': ' + text + '\n')
 
 def job():
