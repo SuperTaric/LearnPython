e1ce1dac190fa9757898d4c48c4d4117957379b6
tmfed2017 <fulina33@163.com>
2023-03-13 16:04:14 +0800

    fastAPi 路径参数

--- fastApi/main.py
+++ fastApi/main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from enum import Enum
+
+class ModelName(str, Enum):
+    alexnet = 'alexnet'
+    resnet = 'resnet'
+    lenet = 'lenet'
 
 app = FastAPI()
 
@@ -13,3 +19,13 @@ async def read_item(item_id: int):
 @app.get("/names/{name}")
 async def read_item(name: str):
     return {"message": "Hello " + name}
+
+@app.get("models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Leaarning FTW"}
+    
+    if model_name.value == 'lenet':
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    
+    return {"model_name": model_name, "message": "Have some residuals"}
