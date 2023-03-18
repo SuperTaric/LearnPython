3b0b93ddfc7bb32255dec6e130a0bb97f9fe549a
SuperTaric <fulina33@163.com>
2023-03-18 19:22:10 +0800

    Update longestpalindroom.py

--- algorithm/longestpalindroom.py
+++ algorithm/longestpalindroom.py
@@ -1,14 +1,14 @@
-def expandAroundCenter(self, s, left, right):
+def expandAroundCenter(s, left, right):
     while left >= 0 and right < len(s) and s[left] == s[right]:
         left -= 1
         right += 1
     return left + 1, right - 1
 
-def longestPalindrome(self, s: str) -> str:
+def longestPalindrome(s: str) -> str:
     start, end = 0, 0
     for i in range(len(s)):
-        left1, right1 = self.expandAroundCenter(s, i, i)
-        left2, right2 = self.expandAroundCenter(s, i, i + 1)
+        left1, right1 = expandAroundCenter(s, i, i)
+        left2, right2 = expandAroundCenter(s, i, i + 1)
         if right1 - left1 > end - start:
             start, end = left1, right1
         if right2 - left2 > end - start:
