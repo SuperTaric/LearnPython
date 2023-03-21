7a19b7ec9063225fadfc24212cdb8104c0999a4a
tmfed2017 <fulina33@163.com>
2023-03-21 11:30:21 +0800

    环形链表

--- algorithm/listNode.py
+++ algorithm/listNode.py
@@ -11,4 +11,22 @@ def getKthFromEnd(head, k):
         fast, slow = fast.next, slow.next
     return slow
 
+"""
+141.环形链表
+快慢指针
+"""
+
+def hasCycle(head):
+    if not head or not head.next:
+        return False
+    slow = head
+    fast = head.next
+
+    while slow != fast:
+        if not fast or not fast.next:
+            return False
+        slow = slow.next
+        fast = fast.next.next
+    return True
+
 
