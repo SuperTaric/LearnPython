630ce2b875b85d7fa25c78d382fad8227d667ea7
tmfed2017 <fulina33@163.com>
2023-03-22 18:03:33 +0800

    fastApi 请求体

--- fastApi/main.py
+++ fastApi/main.py
@@ -1,10 +1,18 @@
+from typing import Union
 from fastapi import FastAPI
 from enum import Enum
+from pydantic import BaseModel
 
 class ModelName(str, Enum):
     alexnet = 'alexnet'
     resnet = 'resnet'
     lenet = 'lenet'
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
 
 app = FastAPI()
 
@@ -29,3 +37,12 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
     
     return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if (item.tax):
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    print(item.name)
+    return item_dict
